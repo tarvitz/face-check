91cc9e53ae6f506f4a5e3a5bd0059487d6863111
Tarvitz <tarvitz@blacklibrary.ru>
2018-12-16 16:32:44 +0300

    [configuration] fixes

--- src/face_check/settings/secrets.py
+++ src/face_check/settings/secrets.py
@@ -4,7 +4,7 @@
 from . utils import get_env_string
 
 SOCIAL_AUTH_TWITCH_KEY = get_env_string('SOCIAL_AUTH_TWITCH_KEY', '')
-SOCIAL_AUTH_TWITCH_SECRET = get_env_string('SOCIAL_AUTH_TWITCH_KEY', '')
+SOCIAL_AUTH_TWITCH_SECRET = get_env_string('SOCIAL_AUTH_TWITCH_SECRET', '')
 
 
 def secrets_dummy():
