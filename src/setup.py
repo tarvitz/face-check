284e7b51b3204ee446513e154e4939fc9aa0c2c4
Tarvitz <tarvitz@blacklibrary.ru>
2019-06-12 15:50:10 +0300

    [vuln] appl django cve vuln update

--- src/setup.py
+++ src/setup.py
@@ -48,7 +48,7 @@ CLASSIFIERS = [
 
 install_requires = [
     #: BSD licenses
-    'Django==2.1.6',
+    'Django==2.1.9',
     'social-auth-app-django==3.1.0',
     'social-auth-core==2.0.0',
     #: MIT licenses
